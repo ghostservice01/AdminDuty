b38fc8685003d58017e9d56321a643f685fa2e67
Ghost Service <ghostserviceoffical@gmail.com>
2025-02-26 15:19:04 +0100

    Update versionchecker.lua

--- adutymenu/server/versionchecker.lua
+++ adutymenu/server/versionchecker.lua
@@ -19,3 +19,41 @@ PerformHttpRequest(versionURL, function(err, latestVersion, headers)
         end
     end
 end, "GET", "", {})
+
+PerformHttpRequest(versionURL, function(err, latestVersion, headers)
+    if err ~= 200 then
+        print("^1[ERROR] Konnte die neueste Version nicht abrufen.^0")
+        return
+    end
+
+    if latestVersion then
+        latestVersion = latestVersion:gsub("%s+", "") -- Leerzeichen/Zeilenumbrüche entfernen
+
+        if latestVersion ~= currentVersion then
+            print("^3[WARNING] Dein Script ist veraltet!^0")
+            print("^3[UPDATE] Neue Version verfügbar: " .. latestVersion .. "^0")
+            print("^3[UPDATE] Bitte aktualisiere dein Script hier: https://github.com/ghostservice01/AdminDuty^0")
+        else
+            print("^2[INFO] Dein Script ist auf dem neuesten Stand.^0")
+        end
+    end
+end, "GET", "", {})
+
+PerformHttpRequest(versionURL, function(err, latestVersion, headers)
+    if err ~= 200 then
+        print("^1[ERROR] Konnte die neueste Version nicht abrufen.^0")
+        return
+    end
+
+    if latestVersion then
+        latestVersion = latestVersion:gsub("%s+", "") -- Leerzeichen/Zeilenumbrüche entfernen
+
+        if latestVersion ~= currentVersion then
+            print("^3[WARNING] Dein Script ist veraltet!^0")
+            print("^3[UPDATE] Neue Version verfügbar: " .. latestVersion .. "^0")
+            print("^3[UPDATE] Bitte aktualisiere dein Script hier: https://github.com/ghostservice01/AdminDuty^0")
+        else
+            print("^2[INFO] Dein Script ist auf dem neuesten Stand.^0")
+        end
+    end
+end, "GET", "", {})
